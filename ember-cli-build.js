3c8d2f201479546971999f1b82da4a59c4208bac
Greg Babiars <gbabiars@gmail.com>
2015-10-06 11:43:34 -0700

    Add d3 to project

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,8 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('bower_components/d3/d3.js');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
